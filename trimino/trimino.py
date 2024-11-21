0ed2c2aedb42d0dbea5e92d34586a6d6dc532048
nathaliaceretta <nathaliagceretta@gmail.com>
2024-11-21 16:39:28 -0300

    started coding a recursive function for solving the matrix

--- trimino/trimino.py
+++ trimino/trimino.py
@@ -28,6 +28,7 @@ print(matriz)
 print(len(matriz))
 
 
+
 def solveSmallBlock(line, col,matriz):
     if(line%2 == 0): #linha par = começo de um bloco minimo
         if(col%2 == 0): # col par = coluna inicial do bloco minimo
@@ -36,8 +37,50 @@ def solveSmallBlock(line, col,matriz):
         else:
             for (x,y) in tiles[2]:
                matriz[line+x][col +y] = 2
+    else:
+        if(col%2 == 0): # col par = coluna inicial do bloco minimo
+           for (x,y) in tiles[4]:
+               matriz[line+x][col +y] = 4
+        else:
+            for (x,y) in tiles[1]:
+               matriz[line+x][col +y] = 1 
 
 
 solveSmallBlock(line,col,matriz)
 
 print(matriz)
+
+def solveMatriz(line, col, size,matriz):
+    centerSquareSize = size/2
+    startDifference = (size - centerSquareSize)/2
+
+    if (size == 2):
+        
+
+    #if centerSquare is not solved, solve center:
+    if(not centerSolved(line + startDifference,col + startDifference, centerSquareSize,matriz)):
+        solveMatriz(line + startDifference,col + startDifference, centerSquareSize,matriz)
+    #else if centerSquare is solved, solve each quadrant:
+        quadrantSize = size/2
+        #solve NW quadrant
+        solveMatriz(line,col,quadrantSize,matriz)
+        #solve SW
+        solveMatriz(line+quadrantSize,col,quadrantSize,matriz)
+        #solve NE
+        solveMatriz(line,col+quadrantSize,quadrantSize,matriz)
+        #solve SE
+        solveMatriz(line+quadrantSize,col+quadrantSize,quadrantSize,matriz)
+
+
+
+def centerSolved(lineStart,colStart,size,matriz):
+    if(matriz[lineStart][colStart] == 0):
+        return False
+    elif (matriz[lineStart +size -1][colStart] == 0):
+        return False
+    elif (matriz[lineStart][colStart + size -1] == 0):
+        return False
+    elif(matriz[lineStart + size -1][colStart +size -1] == 0):
+        return False
+    else:
+        return True
